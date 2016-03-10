9c3db2083dab8124b555beaf86f65b4b9fd011bd
j23schoen <j23schoen@gmail.com>
2016-03-09 21:23:46 -0500

    numbers will add for all 10 frames, no spares or strikes

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,19 @@
 public class Main {
 
-    public int calculate(){
+    int[] rolls = new int[21];
+    int currentRoll = 0;
 
-        return 0;
+    public void addRollsToGame(int pins){
+        rolls[currentRoll++] = pins;
+    }
+    public int calculate(){
+        int total = 0;
+        int frameIndex = 0;
+        for(int i = 0; i < 10; i++){
+            total += rolls[frameIndex] + rolls[frameIndex];
+            frameIndex += 2;
+        }
+
+        return total;
     }
 }
--- test/BowlingTester.java
+++ test/BowlingTester.java
@@ -6,6 +6,9 @@ public class BowlingTester {
     @Test
     public void all1sShouldReturn20(){
         Main game = new Main();
+        for(int i = 0; i < 21; i++){
+            game.addRollsToGame(1);
+        }
         Assert.assertEquals(20, game.calculate());
     }
 
