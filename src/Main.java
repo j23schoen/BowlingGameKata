bde006f49a8d2fc34d7e7c9e6ab09d314e771cff
j23schoen <j23schoen@gmail.com>
2016-03-09 22:13:54 -0500

    changed some method and variable names for readability

--- src/Main.java
+++ src/Main.java
@@ -1,35 +1,35 @@
 public class Main {
 
     int[] rolls = new int[21];
-    int currentRoll = 0;
+    int index = 0;
 
     public void addRollsToGame(int pins){
-        rolls[currentRoll++] = pins;
+        rolls[index++] = pins;
     }
 
-    public int calculate(){
-        int total = 0;
-        int frameIndex = 0;
+    public int findScoreOfBowlingGame(){
+        int gameScore = 0;
+        int framePosition = 0;
 
         for(int i = 0; i < 10; i++){
-            if(checkifStrike(frameIndex)){
-                total += 10 + rolls[frameIndex + 1] + rolls[frameIndex + 2];
-                frameIndex++;
+            if(checkIfAStrikeWasThrown(framePosition)){
+                gameScore += 10 + rolls[framePosition + 1] + rolls[framePosition + 2];
+                framePosition++;
             }
-            else if(checkIfSpare(frameIndex)){
-                total += 10 + rolls[frameIndex + 2];
-                frameIndex += 2;
+            else if(checkIfASpareWasThrown(framePosition)){
+                gameScore += 10 + rolls[framePosition + 2];
+                framePosition += 2;
             }
             else{
-                total += rolls[frameIndex] + rolls[frameIndex + 1];
-                frameIndex += 2;
+                gameScore += rolls[framePosition] + rolls[framePosition + 1];
+                framePosition += 2;
             }
 
         }
-        return total;
+        return gameScore;
     }
 
-    public boolean checkIfSpare(int frameIndex){
+    public boolean checkIfASpareWasThrown(int frameIndex){
         if(rolls[frameIndex] + rolls[frameIndex + 1] == 10){
             return true;
         }
@@ -38,7 +38,7 @@ public class Main {
         }
     }
 
-    public boolean checkifStrike(int frameIndex){
+    public boolean checkIfAStrikeWasThrown(int frameIndex){
         if(rolls[frameIndex] == 10){
             return true;
         }
--- test/BowlingTester.java
+++ test/BowlingTester.java
@@ -16,7 +16,7 @@ public class BowlingTester {
         for(int i = 0; i < 21; i++){
             game.addRollsToGame(1);
         }
-        Assert.assertEquals(20, game.calculate());
+        Assert.assertEquals(20, game.findScoreOfBowlingGame());
     }
 
     //input will be 3, 5, 5, 5, 7, 2, followed by all zeroes
@@ -29,7 +29,7 @@ public class BowlingTester {
         game.addRollsToGame(7);
         game.addRollsToGame(2);
         addRemainingRolls(15, 0);
-        Assert.assertEquals(34, game.calculate());
+        Assert.assertEquals(34, game.findScoreOfBowlingGame());
     }
 
     @Test
@@ -38,7 +38,7 @@ public class BowlingTester {
             game.addRollsToGame(5);
         }
 
-        Assert.assertEquals(150, game.calculate());
+        Assert.assertEquals(150, game.findScoreOfBowlingGame());
     }
 
     //input will be 10, 3, 5, 10, 4, 6, 3, followed by all zeroes
@@ -51,13 +51,13 @@ public class BowlingTester {
         game.addRollsToGame(4);
         game.addRollsToGame(6);
         game.addRollsToGame(3);
-        Assert.assertEquals(62, game.calculate());
+        Assert.assertEquals(62, game.findScoreOfBowlingGame());
     }
 
     @Test
     public void allStrikesShouldReturn300(){
         addRemainingRolls(12, 10);
-        Assert.assertEquals(300, game.calculate());
+        Assert.assertEquals(300, game.findScoreOfBowlingGame());
     }
 
     public void addRemainingRolls(int numberOfRolls, int pins){
